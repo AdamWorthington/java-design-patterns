89ec6c200eb5fd5f5120cd26022719a8263dec7a
Adam Worthington <aaworth111@gmail.com>
2019-07-24 21:42:53 -0700

    Singleton example with early creation.

---
+++ singleton/LoggerSingleton.java
@@ -0,0 +1,16 @@
+class LoggerSingleton{
+
+    private static LoggerSingleton loggerSingleton = new LoggerSingleton();
+
+    private LoggerSingleton(){}  
+      
+    //
+    public static LoggerSingleton getInstance(){  
+     return loggerSingleton;  
+    }  
+     
+    public void log(String s){  
+        System.out.println("Pretending to log: " + s);
+    }  
+
+}
---
+++ singleton/Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main{
+    public static void main(String[] args){
+        LoggerSingleton logger = LoggerSingleton.getInstance();
+        logger.log("hoopla");
+    }
+}
